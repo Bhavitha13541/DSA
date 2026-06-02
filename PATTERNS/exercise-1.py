9d2391454e7f6a30814fc80965ad727c7602bed5
Bhavi13541 <bhavitha281reddy@gmail.com>
2026-06-02 23:02:39 +0530

    pattern1

---
+++ PATTERNS/exercise-1.py
@@ -0,0 +1,28 @@
+# for i in range(5):
+#   for j in range(5):
+#     print("*", end = " ")
+#   print("\n")
+
+
+# * * * * * 
+
+# * * * * * 
+
+# * * * * * 
+
+# * * * * * 
+
+# * * * * * 
+
+n = 3
+for i in range(n):
+  for j in range(n):
+    print("1", end = "")
+  print() 
+
+
+# output
+
+111
+111
+111
