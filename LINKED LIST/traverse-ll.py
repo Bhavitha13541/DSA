d1e19df242fa368eed5cbc14fe65fa344fe4c99d
Bhavi13541 <bhavitha281reddy@gmail.com>
2026-06-04 19:25:09 +0530

    traverse-LL

---
+++ LINKED LIST/traverse-ll.py
@@ -0,0 +1,25 @@
+class Node:
+  def __init__(self, data):
+    self.data = data
+    self.next = None
+
+# Creating nodes
+node1 = Node(10)
+node2 = Node(20)
+node3 = Node(30)
+node4 = Node(40)
+node5 = Node(50)
+
+# Linking nodes
+node1.next = node2
+node2.next = node3
+node3.next = node4
+node4.next = node5
+
+temp = node1
+while temp is not None:
+  print(temp.data,end=" -> ")
+  temp = temp.next
+print("Null")
+
+# 10 -> 20 -> 30 -> 40 -> 50 -> Null
